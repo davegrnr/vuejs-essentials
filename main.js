24fb2f2aaff6be8605cbf718f6c369caa266d43f
davegrnr <davegrnr@gmail.com>
2021-08-27 08:38:46 -0400

    Add several components and click event

---
+++ main.js
@@ -0,0 +1,46 @@
+const app = Vue.createApp({
+    data: function () {
+        return {
+            cls: 'flx',
+            services: [
+                {
+                    title: "Graphics Design & Website",
+                    img: "/assets/graphics.gif",
+                    details: ['Graphics with your brand logo', 'Presense on Google Map', 'Delight Your Customers'],
+                    bg: "#1F75A0",
+                },
+                {
+                    title: "Mobile App Developement",
+                    img: "/assets/mobile.gif",
+                    details: ['Android app befitting your brand ', 'Troubleshooting and Debugging', 'App Maintanance '],
+                    bg: "#fff",
+                },
+                {
+                    title: "Computer Vision And Analytics",
+                    img: "/assets/cv1.gif",
+                    details: ['Awesome Computer vision projects', 'some other stuff', 'some other stuff', 'bla bla bla'],
+                    bg: "#aaa5",
+                },
+                {
+                    title: "Business Consulting",
+                    img: "/assets/vb.jpg",
+                    details: ['Awesome Computer vision projects', 'some other stuff', 'some other stuff', 'bla bla bla'],
+                    bg: "#fff",
+                },
+                {
+                    title: "Custom Solutions",
+                    img: "/assets/custom-cloud.gif",
+                    details: ['Awesome Computer vision projects', 'some other stuff', 'some other stuff', 'bla bla bla'],
+                    bg: "#4279E8",
+                }
+            ]
+        }
+    },
+    methods: {
+        sayHi(){
+            alert("Hi!");
+        }
+    }
+});
+
+const mountedApp = app.mount('#app');
